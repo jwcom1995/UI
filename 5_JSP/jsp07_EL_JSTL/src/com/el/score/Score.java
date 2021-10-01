1634ece2ab9f4f2319537e535b99c363cd6ef03c
jwcom1995 <87286684+jwcom1995@users.noreply.github.com>
2021-10-01 22:07:52 +0900

    Score class

---
+++ 5_JSP/jsp07_EL_JSTL/src/com/el/score/Score.java
@@ -0,0 +1,98 @@
+package com.el.score;
+
+public class Score {
+	private String name;
+	private int kor;
+	private int eng;
+	private int math;
+	private int sum;
+	private double avg;
+	private String grade;
+	
+	public Score() {}
+
+	public Score(String name, int kor, int eng, int math) {
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		setSum();
+		setAvg();
+		setGrade();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getSum() {
+		return sum;
+	}
+
+	public void setSum() {
+		this.sum = kor+eng+math;
+	}
+
+	public double getAvg() {
+		return avg;
+	}
+
+	public void setAvg() {
+		this.avg = getSum()/3;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade() {
+		switch((int)getAvg()/10) {
+		case 10:
+		case 9:
+			grade="A";
+			break;
+		case 8:
+			grade="B";
+			break;
+		case 7:
+			grade="C";
+			break;
+		case 6:
+			grade="D";
+			break;
+		default:
+			grade="F";
+			break;
+		}
+	}
+	
+	
+}
