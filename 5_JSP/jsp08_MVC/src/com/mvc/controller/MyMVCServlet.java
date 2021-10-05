f88854c9c693558f54be03c793d00093e70900b5
jwcom1995 <87286684+jwcom1995@users.noreply.github.com>
2021-10-05 23:20:06 +0900

    controller

--- 5_JSP/jsp08_MVC/src/com/mvc/controller/MyMVCServlet.java
+++ 5_JSP/jsp08_MVC/src/com/mvc/controller/MyMVCServlet.java
@@ -1,6 +1,7 @@
 package com.mvc.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -40,8 +41,62 @@ public class MyMVCServlet extends HttpServlet {
 			
 			request.setAttribute("dto",dto);
 			dispatch("boarddetail.jsp",request,response);
+		} else if (command.equals("writeform")){
+			response.sendRedirect("boardwrite.jsp");			
+		} else if(command.equals("boardwrite")) {
+			String writer = request.getParameter("writer");
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			
+			MyMVCDto dto = new MyMVCDto(writer,title,content);
+			
+			boolean res = biz.insert(dto);
+			
+			if(res) {
+				jsResponse("글 작성 성공","controller.do?command=list",response);
+			} else {
+				jsResponse("글 작성 실패","controller.do?command=writeform",response);
+			}
+		} else if(command.equals("updateform")) {
+			int seq= Integer.parseInt(request.getParameter("seq"));
+			MyMVCDto dto = biz.selectOne(seq);
+			
+			request.setAttribute("dto", dto);
+			dispatch("boardupdate.jsp",request,response);
+		} else if(command.equals("boardupdate")) {
+			int seq=Integer.parseInt(request.getParameter("seq"));
+			String title=request.getParameter("title");
+			String content=request.getParameter("content");
+			
+			MyMVCDto dto = new MyMVCDto(seq,title,content);
+			
+			boolean res = biz.update(dto);
+			
+			if(res) {
+				jsResponse("수정 성공","controller.do?command=detail&seq="+seq,response);
+			} else {
+				jsResponse("수정 실패","controller.do?command=detail&seq="+seq,response);
+			}
+		} else if(command.equals("boarddelete")) {
+			int seq = Integer.parseInt(request.getParameter("seq"));
+			
+			boolean res = biz.delete(seq);
+			
+			if(res) {
+				jsResponse("삭제 성공","controller.do?command=list",response);
+			} else {
+				jsResponse("삭제 실패","controller.do?command=detail&seq="+seq,response);
+			}
 		}
 		
+	}
+	private void jsResponse(String msg,String url, HttpServletResponse response) throws IOException {
+		String s = "<script type='text/javascript'>"
+				+ "alert('"+msg+"');"
+				+ "location.href='"+url+"';"
+				+ "</script>";
+		PrintWriter out = response.getWriter();
+		out.print(s);
 	}
 	
 	//RequestDispatcher 간소화
