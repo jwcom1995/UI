92efcc4ef9ef1c1381d946bffa34fcfd5d350756
jwcom1995 <87286684+jwcom1995@users.noreply.github.com>
2021-10-09 19:58:42 +0900

    add comands

--- 5_JSP/jsp09_AnswerBoard/src/com/answer/controller/AnswerController.java
+++ 5_JSP/jsp09_AnswerBoard/src/com/answer/controller/AnswerController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.answer.biz.AnswerBiz;
 import com.answer.dao.AnswerDao;
 import com.answer.dto.AnswerDto;
 
@@ -45,7 +46,99 @@ public class AnswerController extends HttpServlet {
 			RequestDispatcher dispatch = request.getRequestDispatcher("boarddetail.jsp");
 			dispatch.forward(request, response);
 			
+		} else if (command.equals("writeform")){
+			response.sendRedirect("boardwrite.jsp");
+		
+		} else if (command.equals("boardwrite")) {
+			String writer = request.getParameter("writer");
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			
+			AnswerDto dto = new AnswerDto(writer,title,content);
+			
+			int res = dao.insert(dto);
+			
+			if(res>0) {
+				dispatch("answer.do?command=list",request,response);
+			} else {
+				dispatch("answer.do?command=writeform",request,response);
+			}
+			
+		} else if (command.equals("updateform")) {
+			int boardno = Integer.parseInt(request.getParameter("boardno"));
+			
+			AnswerDto dto = dao.selectOne(boardno);
+			
+			request.setAttribute("dto", dto);
+			
+			dispatch("boardupdate.jsp",request,response);
+			
+		} else if (command.equals("boardupdate")) {
+			int boardno = Integer.parseInt(request.getParameter("boardno"));
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			
+			AnswerDto dto = new AnswerDto();
+			dto.setBoardno(boardno);
+			dto.setTitle(title);
+			dto.setContent(content);
+			
+			int res = dao.update(dto);
+			
+			if(res>0) {
+				dispatch("answer.do?command=list&",request,response);
+			} else {
+				dispatch("answer.do?command=detail&boardno="+boardno,request,response);
+			}
+			
+		} else if (command.equals("delete")) {
+			int boardno = Integer.parseInt(request.getParameter("boardno"));
+			
+			int res= dao.delete(boardno);
+			
+			if(res>0) {
+				dispatch("answer.do?command=list",request,response);
+			}else {
+				dispatch("answer.do?command=detail&boardno="+boardno,request,response);
+			}
+			
+		} else if(command.equals("answerform")) {
+			int parentboardno = Integer.parseInt(request.getParameter("parentboardno"));
+			
+			AnswerDto dto = dao.selectOne(parentboardno);
+			request.setAttribute("parent", dto);
+			dispatch("answerwrite.jsp",request,response);
+						
+		} else if (command.equals("answerwrite")) {
+			int parentboardno = Integer.parseInt(request.getParameter("parentboardno"));
+			
+			String writer =request.getParameter("writer");
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			
+			AnswerDto parent= dao.selectOne(parentboardno);
+			
+			int parentgroupno = parent.getGroupno();
+			int parentgroupsq = parent.getGroupsq();
+			int parenttitletab = parent.getTitletab();
+			
+			AnswerDto dto = new AnswerDto(0,parentgroupno,parentgroupsq,parenttitletab,title,content,writer,null);
+			
+			boolean res = new AnswerBiz().answerLogic(dto);
+			
+			if(res) {
+				response.sendRedirect("answer.do?command=list");
+			} else {
+				response.sendRedirect("answer.do?command=detail&boardno="+parentboardno);
+			}
+			
 		}
+				
+	}
+	
+	private void dispatch(String url, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatch = request.getRequestDispatcher(url);
+		dispatch.forward(request, response);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
